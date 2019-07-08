da77e518f4459731a4d110ff5083d0ab760d4c58
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-07-08 20:46:56 +1000

    created simple post request

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const app = new express()
-const port = process.env.PORT || 5000
-
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, (err) => {
   if(err) {
     console.log('not connected to db')
@@ -13,12 +10,29 @@ mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, (err) => {
   }
 })
 
+const app = new express()
+const port = process.env.PORT || 5000
+const User = require('./models/User')
+
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('hello world from now.sh')
 })
 
-app.get('/hello', (req, res) => {
-  res.send('hello!')
+app.post('/create-user', async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const newUser = new User({
+      username: username,
+      password: password
+    })
+    console.log(newUser)
+    const savedUser = await newUser.save()
+    res.status(200).send({savedUser})
+  } catch (err) {
+    res.status(400).send(err)
+  }
 })
 
 app.listen(port, () => {
