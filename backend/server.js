3567e47e8f547da9adad7bc11684c77fefaef308
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-07-08 19:08:23 +1000

    started to code server.js

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = new express()
+const port = process.env.PORT || 5000
+
+
+app.listen(port, () => {
+  console.log(`listening to ${port}`)
+})
