b71c30a0567d3ca168c9516ff230fbe0f34fa1ce
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-07-08 20:16:36 +1000

    deployed server to now.sh

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,20 @@ require('dotenv').config()
 const app = new express()
 const port = process.env.PORT || 5000
 
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, (err) => {
+  if(err) {
+    console.log('not connected to db')
+  } else {
+    console.log('everything is ok!')
+  }
+})
+
 app.get('/', (req, res) => {
-  res.send({
-    hello: 'hello!!'
-  })
+  res.send('hello world from now.sh')
+})
+
+app.get('/hello', (req, res) => {
+  res.send('hello!')
 })
 
 app.listen(port, () => {
