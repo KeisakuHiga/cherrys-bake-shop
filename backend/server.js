b2e80176b1efb92ea5abb6e75a7fa6b518d4883e
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-07-08 19:16:02 +1000

    added '/hello' endpoint to make sure it works

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,12 @@ require('dotenv').config()
 const app = new express()
 const port = process.env.PORT || 5000
 
+app.get('/', (req, res) => {
+  res.send({
+    hello: 'hello'
+  })
+})
 
 app.listen(port, () => {
-  console.log(`listening to ${port}`)
+  console.log(`listening to port ${port}`)
 })
