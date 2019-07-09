ea3a192b0eb71ac9187ff0f3bda0f7174dd29d2a
SeepG <seep.torque@gmail.com>
2019-07-09 16:06:04 +1000

    dotenv package added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  async componentDidMount() {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/HELLO`);
+    const data = await response.json();
+    console.log(data);
+  }
   render() {
     return <h1>Hello World!</h1>;
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import dotenv from "dotenv";
+dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById("root"));
