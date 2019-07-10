4594db47d9c2da8494f560acaae1e45026213457
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-07-10 12:01:08 +1000

    add routes and controller repository to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 import "./App.css";
+const axios = require('axios')
 
 class App extends React.Component {
-  async componentDidMount() {
-    console.log(process.env);
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/hello`);
-    console.log(response);
-    const data = await response.json();
-    console.log(data);
+  state = {}
+
+  componentDidMount = async () => {
+    const url = process.env.REACT_APP_API_URL
+    const response = await axios.get(`${url}/hello`);
+    console.log(response)
+    // const data = await response.json();
+    this.setState({
+      hello: response.data
+    })
   }
+
   render() {
-    return <h1>Hello World!</h1>;
+    
+    const { hello } = this.state
+    console.log(hello)
+    if(!hello) {
+      return null
+    }
+    return(
+      <h1>{hello.hello}</h1>
+    ) 
   }
 }
 
