59f71ffdfb9257b2eef8ee567c6d358672741660
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-07-11 16:53:47 +1000

    add + err for debugging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,21 @@ import React from "react";
 import Routes from './Routes'
 import "./App.css";
 
-const axios = require('axios')
+// const axios = require('axios')
 
 class App extends React.Component {
   state = {}
 
+  
+
   componentDidMount = async () => {
-    const url = process.env.REACT_APP_API_URL
-    const response = await axios.get(`${url}/hello`);
-    console.log(response)
-    // const data = await response.json();
-    this.setState({
-      hello: response.data
-    })
+    // const url = process.env.REACT_APP_API_URL
+    // const response = await axios.get(`${url}/hello`);
+    // console.log(response)
+    // // const data = await response.json();
+    // this.setState({
+    //   hello: response.data
+    // })
   }
 
   render() {
