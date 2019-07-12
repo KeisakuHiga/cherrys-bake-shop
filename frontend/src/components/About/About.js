2c73b842a2e662e89384f9b5f25696880a3f0d1d
SeepG <seep.torque@gmail.com>
2019-07-12 13:01:45 +1000

    added text

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -4,7 +4,17 @@ class About extends Component {
   render() {
     return (
       <div className="About-container">
-        <p>Hello from KK.js component</p>
+        <h1>Our Story</h1>
+        <p>
+          Named after our lovely child, <strong>Cherry</strong>, we opened The
+          Cherry's Bakeshop and Cafe in 2016. We always had a dream of opening
+          our store.
+          <pre />
+          Mariz used to previously sell cakes and orders from our home. She has
+          attained certifications in baking and filipino cuisine. We at Cherry's
+          Bakeshop and Cafe pride ourselves on delivering unmatched quality
+          cakes since inception.
+        </p>
       </div>
     );
   }
