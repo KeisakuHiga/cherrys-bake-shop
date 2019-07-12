9e46199542aa7329c4bc8cf58c5520ce1b670dc0
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-07-12 11:54:34 +1000

    updated userSchema based on Figma design

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,10 +2,54 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  username:  String,
-  password: String
+  orders: [{ type: Schema.Types.ObjectId, ref: 'Order' }],
+  username: {
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+  },
+  contact: {
+    email: {
+      type: String,
+      required: true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    }
+  },
+  password: String,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 })
 
-const User = mongoose.model('User', userSchema)
+const orderSchema = new Schema({
+  userId: { type: Schema.Types.ObjectId, ref: 'User' },
+  dateOfEvent: ISODate("2016-04-08T15:06:21.595Z"),
+  eventType: "Lorem",
+  pickUpOrDelivery: "Lorem",
+  payment: false,
+  deliveryAddress: "Lorem",
+  numOfServingsRequired: -16,
+  portionSize: "Lorem",
+  designDescription: "Lorem",
+  chosenFlavour: "Lorem",
+  message: "Lorem",
+  whereDidYouHeard: "Lorem",
+  createdAt: ISODate("2016-04-08T15:06:21.595Z"),
+  updatedAt": ISODate("2016-04-08T15:06:21.595Z")
+})
 
-module.exports = User
+
+const User = mongoose.model('User', userSchema)
+const Order = mongoose.model('Order', orderSchema)
+
+module.exports = {
+  User,
+  Order
+}
