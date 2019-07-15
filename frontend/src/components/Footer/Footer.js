a51c78390149d1beb0276b2440de6e441908ed72
SeepG <seep.torque@gmail.com>
2019-07-15 16:45:39 +1000

    footer ready with required info

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -6,12 +6,18 @@ class Footer extends Component {
     return (
       <>
         <div className={style.footercontainer}>
-          <h1 className="footer-text ml-3 text-light">
+          <h1 className="footer-text ml-3 text-dark">
             Cherry's Bakeshop and Cafe
           </h1>
 
-          <h4 className="footer-text ml-3 text-light">
+          <h4 className="footer-text ml-3 text-dark">
             831B Ballarat Rd, Deer Park VIC 3023
+          </h4>
+          <h4 className="footer-text ml-3 text-dark">
+            Tuesdays - Fridays: 10am - 6pm{" "}
+          </h4>
+          <h4 className="footer-text ml-3 text-dark">
+            Saturdays and Sundays: 11am - 5pm
           </h4>
         </div>
       </>
