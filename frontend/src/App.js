75ec02185dedb38f1ad10b1ac656562ea7d7e2be
SeepG <seep.torque@gmail.com>
2019-07-16 14:39:49 +1000

    google map added on contact page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,100 +4,103 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-const axios = require('axios')
+const axios = require("axios");
 
 class App extends React.Component {
   state = {
     allQuotes: null,
     authentication: false
-  }
+  };
 
   componentDidMount = async () => {
     try {
-      this.getAllQuotes()
-      const token = localStorage.getItem('token')
-      const authentication = await axios.get(`${process.env.REACT_APP_API_URL}/user/current-user`, { headers: { token: token } })
+      this.getAllQuotes();
+      const token = localStorage.getItem("token");
+      const authentication = await axios.get(
+        `${process.env.REACT_APP_API_URL}/user/current-user`,
+        { headers: { token: token } }
+      );
       this.setState({
         authentication: true,
         currentUser: authentication.data
-      })
-    } catch(err) {
+      });
+    } catch (err) {
       this.setState({
         authentication: false
-      })
+      });
     }
-  }
+  };
 
   getAllQuotes = async () => {
-    const url = `${process.env.REACT_APP_API_URL}/quote/getAllQuotes`
-    const response = await axios.get(url)
-    const data = await response.data
+    const url = `${process.env.REACT_APP_API_URL}/quote/getAllQuotes`;
+    const response = await axios.get(url);
+    const data = await response.data;
     this.setState({
       allQuotes: data
-    })
-  }
+    });
+  };
 
-  register = async (userInfo) => {
-    const url = process.env.REACT_APP_API_URL
+  register = async userInfo => {
+    const url = process.env.REACT_APP_API_URL;
     try {
-      const response =  await axios.post(`${url}/auth/register`, userInfo)
-      const token = response.data.token
-      localStorage.setItem("token", token)
+      const response = await axios.post(`${url}/auth/register`, userInfo);
+      const token = response.data.token;
+      localStorage.setItem("token", token);
       this.setState({
         authentication: true
-      })
+      });
     } catch (err) {
       this.setState({
         authentication: false,
         errorMessage: `Error => ${err.message}`
-      })
+      });
     }
-  }
+  };
 
-  login = async (userCredentials) => {
-    const url = process.env.REACT_APP_API_URL
+  login = async userCredentials => {
+    const url = process.env.REACT_APP_API_URL;
     try {
-      const response =  await axios.post(`${url}/auth/login`, userCredentials)
-      const token = response.data.token
-      localStorage.setItem("token", token)
+      const response = await axios.post(`${url}/auth/login`, userCredentials);
+      const token = response.data.token;
+      localStorage.setItem("token", token);
       this.setState({
         authentication: true
-      })
+      });
     } catch (err) {
       this.setState({
         authentication: false,
         errorMessage: `Wrong credential ${err.message}`
-      })
+      });
     }
-  }
+  };
 
-  dateFormat = (date) => {
-    date = new Date(date)
-    const month = date.toLocaleString("en-us", { month: "short" })
-    const day = date.getDate()
-    const year = date.getFullYear()
-    const newDate = month+'. '+day+', ' + year
-    return newDate
-  }
+  dateFormat = date => {
+    date = new Date(date);
+    const month = date.toLocaleString("en-us", { month: "short" });
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const newDate = month + ". " + day + ", " + year;
+    return newDate;
+  };
 
   render() {
-    const { allQuotes, authentication } = this.state
-    if(!allQuotes) {
-      return null
+    const { allQuotes, authentication } = this.state;
+    if (!allQuotes) {
+      return null;
     } else {
       return (
         <div>
           <Navbar />
-          <Routes 
-            allQuotes={allQuotes} 
-            authentication={authentication} 
-            dateFormat={this.dateFormat} 
-            register={this.register} 
-            login={this.login} 
+          <Routes
+            allQuotes={allQuotes}
+            authentication={authentication}
+            dateFormat={this.dateFormat}
+            register={this.register}
+            login={this.login}
           />
           <Footer />
         </div>
-      )
+      );
     }
   }
 }
--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 class Contact extends Component {
   state = {};
   render() {
-    return <h1>Hello from Contact.js component</h1>;
+    return (
+      <>
+        <div className="googleMapcontainer">
+          <iframe
+            width="600"
+            height="450"
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12615.399763966592!2d144.7718444!3d-37.7701166!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa2502533f29066!2sCherry&#39;s+Bake+Shop+%26+Cafe!5e0!3m2!1sen!2sau!4v1563250557464!5m2!1sen!2sau"
+            allowFullScreen
+          />
+        </div>
+      </>
+    );
   }
 }
 
