a92e7387cbf39f732491b608c6c5fbe2fb55ead6
rommelryanrico <rommelryanrico@hotmail.com>
2019-07-17 12:11:16 +1000

    finished contact page

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react"
 import { Redirect } from 'react-router-dom'
+import style from '../Login/Login.module.css'
+
 
 class Login extends Component {
   state = {}
@@ -20,26 +22,28 @@ class Login extends Component {
       return <Redirect to="/" />
     } else {
       return (
-        <>
-          <form>
-            <label>Email</label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              onChange={this.handleInput}
-            />
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              onChange={this.handleInput}
-            />
-            <input type="submit" value="Submit" onClick={this.handleLogin} />
-          </form>
-          {this.state.errorMessage && <h1>{this.state.errorMessage}</h1>}
-        </>
+        <div className={style.logincontainer}>
+          <div className={style.logininner}>
+            <form className={style.loginform}>
+              <label>Email</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                onChange={this.handleInput}
+              />
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                onChange={this.handleInput}
+              />
+              <input type="submit" value="Submit" onClick={this.handleLogin} />
+            </form>
+            {this.state.errorMessage && <h1>{this.state.errorMessage}</h1>}
+          </div>
+        </div>
       )
     }
   }
