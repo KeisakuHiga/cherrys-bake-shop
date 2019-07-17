31e42c0afe21a7044607f209d5c89df4f7935921
KeisakuHiga <iamsakufromokinawa@yahoo.co.jp>
2019-07-17 10:35:28 +1000

    installed react-phone-number-input

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -16,10 +16,11 @@ class Admin extends Component {
             <thead>
               <tr>
                 <th scope="col">#</th>
-                <th scope="col">User First Name</th>
-                <th scope="col">User Last Name</th>
-                <th scope="col">Event Date</th>
-                <th scope="col">Quote Request Date</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Phone Number</th>
+                <th scope="col">Estimated Pick Up</th>
+                <th scope="col">Created At</th>
               </tr>
             </thead>
             <tbody>
@@ -28,7 +29,8 @@ class Admin extends Component {
                   <th key={quote._id} scope="row"><Link to={`/QuoteDetail/${quote._id}`}>Quote Detail</Link></th>
                   <td key={quote.user.userName.firstName}>{quote.user.userName.firstName}</td>
                   <td key={quote.user.userName.lastName}>{quote.user.userName.lastName}</td>
-                  <td key={quote.createdAt}><Moment local format="MMM DD, YYYY">{quote.pickUp.time}</Moment></td>
+                  <td key={quote.user.contact.phoneNumber}>{quote.user.contact.phoneNumber}</td>
+                  <td key={quote.pickUp.time}><Moment local format="MMM DD, YYYY">{quote.pickUp.time}</Moment></td>
                   <td key={quote.createdAt}><Moment local format="MMM DD, YYYY">{quote.createdAt}</Moment></td>
                 </tr>
               ))}
