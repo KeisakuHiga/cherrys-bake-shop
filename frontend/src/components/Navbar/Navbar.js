92d9415d160272c1e2e7e9339fc7f22c8623017b
SeepG <seep.torque@gmail.com>
2019-07-18 16:44:37 +1000

    no match page set-up

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,55 +1,80 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import style from '../Navbar/Navbar.module.css'
-import CherrysLogo from '../Navbar/cherryslogo.png'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import style from "../Navbar/Navbar.module.css";
+import CherrysLogo from "../Navbar/cherryslogo.png";
 
 class Navbar extends Component {
-  state = {}
+  state = {};
 
-  handleLogout = (e) => {
-    e.preventDefault()
-    this.props.logout()
-  }
+  handleLogout = e => {
+    e.preventDefault();
+    this.props.logout();
+  };
 
-  render() { 
-    const { authentication } = this.props
+  render() {
+    const { authentication } = this.props;
     return (
       <>
         <div className={style.logocontainer}>
-          <img className={style.logoimage} src={CherrysLogo} alt="Cherry's Logo"/>
+          <img
+            className={style.logoimage}
+            src={CherrysLogo}
+            alt="Cherry's Logo"
+          />
         </div>
         <nav className={style.navbar}>
-            <ul className={style.navbox}>
+          <ul className={style.navbox}>
+            <li className={style.navitem}>
+              <Link className={style.navlink} to="/">
+                Home
+              </Link>
+            </li>
+            <li className={style.navitem}>
+              <Link className={style.navlink} to="/About">
+                About
+              </Link>
+            </li>
+            <li className={style.navitem}>
+              <Link className={style.navlink} to="/Quote">
+                Quote
+              </Link>
+            </li>
+            <li className={style.navitem}>
+              <Link className={style.navlink} to="/Faq">
+                FAQ
+              </Link>
+            </li>
+            <li className={style.navitem}>
+              <Link className={style.navlink} to="/Contact">
+                Contact
+              </Link>
+            </li>
+            {authentication ? (
               <li className={style.navitem}>
-                <Link className={style.navlink} to="/">Home</Link>
+                <Link className={style.navlink} to="/Dashboard">
+                  Dashboard
+                </Link>
               </li>
+            ) : null}
+            {authentication ? null : (
               <li className={style.navitem}>
-                <Link className={style.navlink} to="/About">About</Link>
+                <Link className={style.navlink} to="/Login">
+                  Login
+                </Link>
               </li>
-              <li className={style.navitem}>
-                <Link className={style.navlink} to="/Quote">Quote</Link>
+            )}
+            {authentication ? (
+              <li className={style.navitem} onClick={this.handleLogout}>
+                <Link className={style.navlink} to="/">
+                  Logout
+                </Link>
               </li>
-              <li className={style.navitem}>
-                <Link className={style.navlink} to="/Faq">FAQ</Link>
-              </li>
-              <li className={style.navitem}>
-                <Link className={style.navlink} to="/Contact">Contact</Link>
-              </li>
-              {authentication ? <li className={style.navitem}>
-                <Link className={style.navlink} to="/Dashboard">Dashboard</Link>
-              </li> : null }
-              {authentication ? null : <li className={style.navitem}>
-                <Link className={style.navlink} to="/Login">Login</Link>
-              </li> } 
-              {authentication ? <li className={style.navitem} onClick={this.handleLogout}>
-                <Link className={style.navlink} to="/">Logout</Link>
-              </li> : null }
-            </ul>
+            ) : null}
+          </ul>
         </nav>
-       
       </>
-    )
+    );
   }
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/components/Quote/Quote.js
+++ frontend/src/components/Quote/Quote.js
@@ -1,15 +1,10 @@
-import React, { Component } from 'react'
-import style from "../Quote/Quote.module.css"
+import React, { Component } from "react";
+import style from "../Quote/Quote.module.css";
 
 class Quote extends Component {
-  state = {  }
-  render() { 
-    return (
-      <div className={style.quotecontainer}>
-        
-      </div>
-
-    )
+  state = {};
+  render() {
+    return <div className={style.quotecontainer} />;
   }
 }
 
