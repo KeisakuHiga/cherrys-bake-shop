73b700a2d8edc20bb094e60972cde2ac7d04aa1e
SeepG <seep.torque@gmail.com>
2019-07-19 16:01:29 +1000

    initial Seed Data test

--- backend/__tests__/server.test.js
+++ backend/__tests__/server.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("../api");
+const { createFakeData } = require("../utils/utils");
 
 // Checks if API is working
 describe("api is working", () => {
@@ -9,18 +10,23 @@ describe("api is working", () => {
   });
 });
 
-// Checks if SeedData is seeding data
-describe("POST /seedData", () => {
-  test('api sends 200 for "localhost:5000/quote/seedData/" endpoint', async () => {
-    request(app).post();
-    const name = "keisaku";
-    const password = "password";
+describe("testing seed data", () => {
+  test("createFakeData() should return an array of 10 documents", () => {
+    const result = createFakeData(20);
+    expect(result.length).toBe(20);
+  });
+});
+
+// Checks incorrect username and password token logic
+
+describe("testing authentication and token logic", () => {
+  test("testing if '/login' sends back no token when incorrect username and password", async () => {
     const response = await request(app)
-      .post("quote/seedData")
+      .post("/auth/login")
       .send({
-        name: name,
-        password: password
+        username: "seep@gmail.com",
+        password: "password"
       });
-    expect(response.text).toBe(200);
+    expect(response.status).toBe(403);
   });
 });
