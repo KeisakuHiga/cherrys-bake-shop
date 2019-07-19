d49cc9aae9b9b0d889dbacab03687a1648a72248
SeepG <seep.torque@gmail.com>
2019-07-19 12:56:07 +1000

    backend seed data test

--- backend/__tests__/server.test.js
+++ backend/__tests__/server.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../api");
 
-// describe
+// Checks if API is working
 describe("api is working", () => {
   test('api sends 200 for "localhost:5000/quote/" endpoint', async () => {
     const response = await request(app).get("/quote/");
@@ -9,29 +9,50 @@ describe("api is working", () => {
   });
 });
 
-// describe("test is ok", async () => {
-//   test("check that / send back api is working", async () => {
-//     const response = await request(app).get("/");
-//     // console.log(response.text)
-//     expect(response.text).toBe("api working");
-//   });
-//   test('check that "/" sends back a 200 if the server is up', async () => {
-//     const response = await request(app).get("/");
-//     expect(response.status).toBe(200);
-//   });
-// });
+// http://localhost:5000/quote/seedData
 
-// describe('testing the post endpoints "/create-data" works in the way we expect', async () => {
-//   test("that the endpoint sends back the name that we pass as a parameter in the request body", async () => {
-//     const name = "keisaku";
-//     const response = await request(app)
-//       .post("/create-data")
-//       .send({
-//         name: name,
-//         password: password
-//       });
-//     expect(response.text).toBe(name);
-//   });
+describe("testing the post endpoint for seeding quote works in the way we expect", async () => {
+  test("api sends 200 for "localhost:5000/quote/seedData" endpoint, async () => {
+    const name = "keisaku";
+    const response = await request(app)
+      .post("/create-data")
+      .send({
+        name: name,
+        password: password
+      });
+    expect(response.text).toBe(200);
+  });
+
+
+  const newUser = await new User({
+    userName: {
+      firstName: data.userData.firstName,
+      lastName: data.userData.lastName
+    },
+    contact: {
+      email: data.userData.email,
+      phoneNumber: data.userData.phoneNumber
+    }
+  })
+  const savedUser = await newUser.save()
+  const newQuote = await new Quote({
+    typeOfProduct: data.quoteData.typeOfProduct,
+    dateOfEvent: data.quoteData.dateOfEvent,
+    pickUp: {
+      date: data.quoteData.pickUpDate,
+      time: data.quoteData.pickUpTime
+    },
+    typeOfOccasion: data.quoteData.typeOfOccasion,
+    numberOfGuests: data.quoteData.numberOfGuests,
+    flavour: {
+      cakeFlavour: data.quoteData.cakeFlavour,
+      fillingFlavour: data.quoteData.fillingFlavour
+    },
+    message: data.quoteData.message,
+    user: savedUser._id
+
+
+
 
 //   test("if request body has worng parameter send an error 404", async () => {
 //     const name = "keisaku";
