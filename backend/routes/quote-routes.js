b9039a4b6c4a04ab61fa48675e0d7e6de4c1efe4
SeepG <seep.torque@gmail.com>
2019-07-19 11:00:14 +1000

    add latest

--- backend/routes/quote-routes.js
+++ backend/routes/quote-routes.js
@@ -1,32 +1,23 @@
-<<<<<<< HEAD
 const express = require("express");
 const router = express.Router();
 const {
   getAllQuotes,
   getOneQuote,
-  createNewQuote
+  createNewQuote,
+  seedFakeData
 } = require("../controllers/quotesController");
 
-router.get("/getAllQuotes", getAllQuotes);
-router.get("/:id", getOneQuote);
-router.post("/newQuote", createNewQuote);
-=======
-const express = require('express')
-const router = express.Router()
-const { getAllQuotes, getOneQuote, createNewQuote, seedFakeData } = require('../controllers/quotesController')
-
-const { checkAccessToken } = require('../utils/utils.js')
+const { checkAccessToken } = require("../utils/utils.js");
 
 // non protected endpoints
-router.post('/newQuote', createNewQuote)
-router.post('/seedData', seedFakeData)
+router.post("/newQuote", createNewQuote);
+router.post("/seedData", seedFakeData);
 
- // this middleware checks whether or not a user provides an access token
-router.use(checkAccessToken)
+// this middleware checks whether or not a user provides an access token
+router.use(checkAccessToken);
 
 // protected endpoints
-router.get('/getAllQuotes', getAllQuotes)
-router.get('/:id', getOneQuote)
->>>>>>> master
+router.get("/getAllQuotes", getAllQuotes);
+router.get("/:id", getOneQuote);
 
 module.exports = router;
