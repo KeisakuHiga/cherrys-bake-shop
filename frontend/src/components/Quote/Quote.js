c0a65991f52929005b162b24039385e80be56515
SeepG <seep.torque@gmail.com>
2019-07-23 16:35:20 +1000

    working on readMe, added workflow images

--- frontend/src/components/Quote/Quote.js
+++ frontend/src/components/Quote/Quote.js
@@ -35,7 +35,6 @@ class Quote extends Component {
               <div className={style.quotecontent}>
                 <div>
                   <p>We are based in Deer Park.</p>
-<<<<<<< HEAD
                   <p>
                     If you are after a quote for a custom made cake or filipino
                     order, please fill out the form below and I will get back to
@@ -48,17 +47,11 @@ class Quote extends Component {
                     </a>
                   </p>
                   <p>
-                    Download cherrys Bakeshop and Cafe cake and filipino food
-                    menu{" "}
+                    Download my filipino food list and cake flavour list{" "}
                     <a href="https://drive.google.com/file/d/10feovHfpQSJTftvNRoH3oeT3SOhDqzfx/view">
-                      here.
+                      here
                     </a>
                   </p>
-=======
-                  <p>If you are after a quote for a custom made cake or filipino order, please fill out the form below and I will get back to you as soon as possible.</p>
-                  <p>You can also email us directly at: <a href="mailto:cherrysbakeshopandcafe@gmail.com">cherrysbakeshopandcafe@gmail.com</a></p>
-                  <p>Download my filipino food list and cake flavour list <a href="https://drive.google.com/file/d/10feovHfpQSJTftvNRoH3oeT3SOhDqzfx/view">here</a></p>
->>>>>>> master
                 </div>
               </div>
               <div>
@@ -115,17 +108,13 @@ class Quote extends Component {
                 </div>
                 <div>
                   <label>
-<<<<<<< HEAD
                     <input
                       type="checkbox"
                       name="typeOfProduct"
                       id="typeOfProduct"
-                      value="Filipino Cuisine"
+                      value="Filipino Cuisine/Dessert"
                       onChange={this.handleInput}
                     />
-=======
-                    <input type="checkbox" name="typeOfProduct" id="typeOfProduct" value="Filipino Cuisine/Dessert" onChange={this.handleInput}/>
->>>>>>> master
                     <span> Filipino Cuisine/Dessert</span>
                   </label>
                 </div>
